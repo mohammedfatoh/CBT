b85dc6e27bc2259200de0f330df279ec331c70cc
mohammedfatoh <mohammedfatoh0@gmail.com>
2022-02-19 01:20:26 +0200

    examination-pages

---
+++ CBT/Controllers/PatientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CBT.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Examination()
+        {
+            return View();
+        }
+
+        public IActionResult PreviosExamination()
+        {
+            return View();
+        }
+    }
+}
