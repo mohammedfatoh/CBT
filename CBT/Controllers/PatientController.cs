986b660d4cf98bf4723997ddc0dac962eea34f1b
mohammedfatoh <mohammedfatoh0@gmail.com>
2022-06-24 18:28:17 +0200

    add new page for result

--- CBT/Controllers/PatientController.cs
+++ CBT/Controllers/PatientController.cs
@@ -2,6 +2,14 @@
 using CBT.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Patagames.Ocr;
+using Patagames.Ocr.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
 
 namespace CBT.Controllers
 {
@@ -50,6 +58,89 @@ namespace CBT.Controllers
 
             return View();
         }
+
+        public IActionResult ExaminationByImage()
+        {
+            return View("Exmination");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ExaminationByImage(Eximination exmination, string IdUser)
+        {
+            if (!ModelState.IsValid)
+                return View(exmination);
+            try
+            {
+                string userId = IdUser;
+                Patient patient = _context.Patients.FirstOrDefault(x => x.UserId == userId);
+                if (patient != null)
+                {
+                    exmination.patient_Id = patient.Id;
+                }
+
+                List<string> word = new List<string>();
+                using (var api = OcrApi.Create())
+                {
+                    api.Init(Languages.English);
+                    string plainText = api.GetTextFromImage("C:\\Users\\Microsoft\\OneDrive\\Desktop\\fff.jpg");
+                    Console.WriteLine(plainText);
+                    string rBC = "", redBloodCell = "", whitebloodCell = "", platelets = "";
+                    for (int ind = 0; ind < plainText.Length; ind++)
+                    {
+                        if (char.IsLetter(plainText[ind]) || char.IsDigit(plainText[ind]))
+                        {
+                            rBC += char.ToLower(plainText[ind]);
+
+                        }
+                        else if (plainText[ind] == '.') { }
+                        else if (rBC.Length > 0)
+                        {
+                            word.Add(rBC);
+                            rBC = "";
+
+                        }
+                    }
+                    for (int ind = 0; ind < word.Count; ind++)
+                    {
+                        if (ind + 2 < word.Count && (word[ind] == "red" || word[ind] == "r.b.cs" || word[ind] == "rbcs"))
+                        {
+                            ind += 3;
+                            redBloodCell = word[ind];
+                        }
+                        if (word[ind] == "rdw")
+                        {
+                            whitebloodCell = word[ind + 1];
+                        }
+                        if (ind + 1 < word.Count && word[ind] == "platelet"
+                             && word[ind + 1] == "count")
+                        {
+                            ind += 2;
+                            platelets = word[ind];
+                        }
+                    }
+                    exmination.RBCS= (float)Convert.ToDouble(redBloodCell); 
+                    exmination.WBES= (float)Convert.ToDouble(whitebloodCell);
+                    exmination.PLT = (float)Convert.ToDouble(platelets);
+                    await _context.Eximinations.AddAsync(exmination);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("", ex.Message);
+                return View("Examination", exmination);
+            }
+
+            return View("Examination");
+        }
+
+        public IActionResult ResultExamination()
+        {
+            return View();
+        }
+
 
         public IActionResult PreviosExamination()
         {
--- CBT/Program.cs
+++ CBT/Program.cs
@@ -33,6 +33,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseMigrationsEndPoint();
+    
 }
 else
 {
